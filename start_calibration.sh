1a4498fa6930a544acac1e1295a00d0e651523ae
MortyL <mortyl0834@gmail.com>
2023-09-14 18:04:55 +0800

    refine calibration process

--- start_calibration.sh
+++ start_calibration.sh
@@ -1,4 +1,17 @@
-BAG_PATH=/data/omnicam/omni_calibration_CAMABCD-split.bag
+BAG_PATH=/data/drone_0_cam/cam_abcd/omni_calibration_2023-09-13-22-26-53-split.bag
+TARGET_PATH=/data/target/april_6x6.yaml
+OUTPUT_PATH=/data/drone_0_cam/cam_abcd/output
+
+# rosrun kalibr tartan_calibrate   --bag $BAG_PATH\
+#   --target $TARGET_PATH\
+#   --topics  /oak_ffc_4p/image_CAM_D/compressed \
+#   --min-init-corners-autocomplete 30 \
+#   --min-tag-size-autocomplete 2 \
+#   --models omni-radtan\
+#   --save_dir $OUTPUT_PATH \
+
+
+# BAG_PATH=/data/omnicam/omni_calibration_CAMABCD-split.bag
 
 rosrun kalibr tartan_calibrate   --bag $BAG_PATH\
   --target /data/target/april_6x6.yaml\
@@ -9,5 +22,5 @@ rosrun kalibr tartan_calibrate   --bag $BAG_PATH\
   --min-init-corners-autocomplete 30 \
   --min-tag-size-autocomplete 2 \
   --models omni-radtan omni-radtan omni-radtan omni-radtan\
-  --save_dir /data/output \
-  --intrinsic-prarameters /catkin_ws/src/kalibr/intrinsic_parameters_for_test.yaml
+  --save_dir $OUTPUT_PATH \
+  --intrinsic-prarameters /data/drone_0_cam/cam_abcd/intrinsic_parameters.yaml
